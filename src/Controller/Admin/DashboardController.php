8e7749d2c5c50e3fcc1f3b7288d95e879d5db7ed
timothee delacretaz <timothee.delacretaz@jobtrek.ch>
2023-10-03 08:30:00 +0200

    start backend home page

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -6,6 +6,7 @@ use App\Entity\Article;
 use App\Entity\Comment;
 use App\Entity\User;
 use App\Entity\Vote;
+use App\Repository\ArticleRepository;
 use App\Repository\UserRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Assets;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
@@ -23,38 +24,109 @@ class DashboardController extends AbstractDashboardController
     public function __construct(
         private ChartBuilderInterface $chartBuilder,
         private UserRepository $userRepository,
+        private ArticleRepository $articleRepository,
     ) {
     }
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
+        $format = 'm';
         $r = $this->userRepository->findUserBetweenDate('2023-01-01 01:01:01','2023-12-30 01:01:01' );
+        $labels = [];
+        $data = [];
 
+        foreach ($r as $x){
+            if (in_array($x->getDate()->format($format),$labels)){
+                $data[array_search($x->getDate()->format($format),$labels)]+=1;
+
+            }else{
+                $labels[] = $x->getDate()->format($format);
+                $data[] = 1;
+            }
+
+        }
         $chart = $this->chartBuilder->createChart(Chart::TYPE_LINE);
         $chart->setData([
-            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July','August','Septembre','October','November','December'],
+            'labels' => $labels,
             'datasets' => [
                 [
-                    'label' => "Nombre d'User",
+                    'label' => "User",
                     'backgroundColor' => 'rgb(255, 180, 132)',
                     'borderColor' => 'rgb(255, 99, 132)',
-                    'data' => [0, 10, 5, 2, 20, 30, 45],
+                    'data' => $data,
                 ],
             ],
         ]);
 
         $chart->setOptions([
+            'responsive' =>'true',
             'scales' => [
 
                 'y' => [
                     'suggestedMin' => 0,
-                    'suggestedMax' => 100,
+                    'suggestedMax' => max($data),
+                    'grid' => [
+                        'color' => 'rgb(0, 0, 0)'
+                    ]
+                ],
+                'x' => [
+                    'grid' => [
+                        'color' => 'rgb(0, 0, 0)'
+                    ]
+                ],
+            ],
+        ]);
+
+
+        $r2 = $this->articleRepository->findArticleBetweenDate('2023-01-01 01:01:01','2023-12-30 01:01:01' );
+        $labels2 = [];
+        $data2 = [];
+
+        foreach ($r2 as $x){
+            if (in_array($x->getDate()->format($format),$labels2)){
+                $data2[array_search($x->getDate()->format($format),$labels2)]+=1;
+
+            }else{
+                $labels2[] = $x->getDate()->format($format);
+                $data2[] = 1;
+            }
+
+        }
+        $chart2 = $this->chartBuilder->createChart(Chart::TYPE_LINE);
+        $chart2->setData([
+            'labels' => $labels2,
+            'datasets' => [
+                [
+                    'label' => "Article",
+                    'backgroundColor' => 'rgb(255, 180, 132)',
+                    'borderColor' => 'rgb(255, 99, 132)',
+                    'data' => $data2,
+                ],
+            ],
+        ]);
+
+        $chart2->setOptions([
+            'responsive' =>'true',
+            'scales' => [
+
+                'y' => [
+                    'suggestedMin' => 0,
+                    'suggestedMax' => max($data2),
+                    'grid' => [
+                        'color' => 'rgb(0, 0, 0)'
+                    ]
+                ],
+                'x' => [
+                    'grid' => [
+                        'color' => 'rgb(0, 0, 0)'
+                    ]
                 ],
             ],
         ]);
 
         return $this->render('admin_dashboard/index.html.twig',[
             'chart' => $chart,
+            'chart2' => $chart2,
         ]);
 
 
